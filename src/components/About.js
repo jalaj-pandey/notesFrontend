8496d2b3186bbb938e966f01eb2eba8f5378ec4d
Jalaj Pandey <jalajpandey123@gmail.com>
2023-11-01 19:34:41 +0530

    updated commit

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,8 @@ import Footer from './Footer';
 import '../styles/About.css'
 import awesome from '../images/about - awesome.jpeg'
 import login from '../images/about - awesome.svg'
+import jalaj from '../images/jalaj-dhoti.jpg'
+import jalajp from '../images/me.png'
 import { Link } from "react-router-dom";
 import { Button } from '@mui/material';
 
@@ -23,7 +25,7 @@ const About = () => {
                 <div className="row">
                     <div className="col-md-6 d-flex flex-column justify-content-center">
                         <h2 className="mb-3" style={{ fontWeight: "Bold" }}>Make something <span style={{ color: "#2A8387" }}>Awesome</span> </h2>
-                        <p >iNotebook is made from the pain of writing all the things in notebook which is very hectic :(, So we mad an online web platform where you can create, edit, upload, delete your notes/information privately and securely without any disturbancee.
+                        <p >ideaSpotter is made from the pain of writing all the things in notebook which is very hectic :(, So we mad an online web platform where you can create, edit, upload, delete your notes/information privately and securely without any disturbancee.
                             you can also access your notes anywhere in your world, at anytime time . So dont forget to Create note because creating anything is always important
                         </p>
                         <div className="d-flex justify-content-center mt-2">
@@ -42,7 +44,7 @@ const About = () => {
                     <div className="col-md-6 d-flex flex-column justify-content-center">
                         <h2 className="mb-3" style={{ fontWeight: "Bold" }}>Powering the <span style={{ color: "#2A8387" }}>internet's visuals</span> </h2>
                         <p>
-                            How we started? The concept was simple. iNotebook was born from the pain of writing all the things in notebook which is very hectic :( .
+                            How we started? The concept was simple. ideaSpotter was born from the pain of writing all the things in notebook which is very hectic :( .
                             An online web platform where you can create, edit, upload, delete your notes/information privately and securely without any disturbancee
                         </p>
                         <div className="d-flex justify-content-center mt-3">
@@ -50,8 +52,29 @@ const About = () => {
                         </div>
                     </div>
                 </div>
-            </div>
 
+                {/* About section */}
+                <div className="container mt-5 ">
+                <div className="row">
+                    <div className="col-md-6 d-flex flex-column justify-content-center">
+                        <h2 className="mb-3" style={{ fontWeight: "Bold" }}>About <span style={{ color: "#2A8387" }}>Jalaj Pandey</span> </h2>
+                        <p >Jalaj Pandey, a 22-year-old student, is currently dedicatedly pursuing his Master's in Computer Applications (MCA) from Kamla Nehru Institute of Technology (KNIT).
+What sets Jalaj apart is his enthusiasm for practical applications of his knowledge. His ability to think critically and apply his knowledge in creative ways has enabled him to develop projects that are not just academically sound but also have practical utility.
+                        </p>
+                        <div className="d-flex justify-content-center mt-2">
+                            <Button className='button' component={Link} to="https://jalajpandey.netlify.app/" variant="contained"  style={{ color: "White", textTransform: "none", fontFamily: "'Poppins', sans-serif", fontSize: "1.3rem" }}>More About Jalaj</Button>
+                        </div>
+                    </div>
+                    <div className="col-md-6 d-flex flex-column justify-content-center">
+                    <div className="d-flex justify-content-center mb-3" >
+                        <img style={{height:"20rem", scale:"1"}} className='img-fluid img-thumbnail'  src={jalaj} alt="about-awesome" />
+                        <img style={{height:"20rem", scale:"1"}} className='img-fluid img-rounded'  src={jalajp} alt="about-awesome" />
+                    </div>
+                    </div>
+                </div>
+                {/* End about section */}
+            </div>
+        </div>
       <Footer/>
     </div>
   )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,11 +12,11 @@ const Footer = () => {
                             <span className="logo_name"><span style={{ color: "#2A8387" }}>Idea</span>Spotter</span>
                         </div>
                         <div className="media-icons">
-                            <Link to="/"><i className="fab fa-facebook-f"></i></Link>
-                            <Link to="/"><i className="fab fa-twitter"></i></Link>
-                            <Link to="/"><i className="fab fa-instagram"></i></Link>
-                            <Link to="/"><i className="fab fa-linkedin-in"></i></Link>
-                            <Link to="/"><i className="fab fa-youtube"></i></Link>
+                            <Link to="https://www.facebook.com/jalajpandey15"><i className="fab fa-facebook-f"></i></Link>
+                            <Link to="https://twitter.com/thejalajpandey"><i className="fab fa-twitter"></i></Link>
+                            <Link to="https://www.instagram.com/thejalajpandey/"><i className="fab fa-instagram"></i></Link>
+                            <Link to="https://www.linkedin.com/in/jalaj-pandey/"><i className="fab fa-linkedin-in"></i></Link>
+                            <Link to="https://www.youtube.com/@JalajPandey/"><i className="fab fa-youtube"></i></Link>
                         </div>
                     </div>
                     <div className="link-boxes">
@@ -34,15 +34,15 @@ const Footer = () => {
                         <ul className="box">
                             <li className="link_name">Account</li>
                             <li><Link to="/login">Sign-in</Link></li>
-                            <li><Link to="/signin">Join Free</Link></li>
+                            <li><Link to="/signup">Join Free</Link></li>
                         </ul>
                         <ul className="box">
-                            <li className="link_name">Top Categories</li>
-                            <li><Link to="/c/61554bfe801949ad7b9be4ff">Tent Notes</Link></li>                            <li><Link to="/c/61554c43d2a6b15f764aff36">Water Notes</Link></li>
-                            <li><Link to="c/61554c63dfd6a37d71449b5c">Survivalist Notes</Link></li>
+                            <li className="link_name">Other Projects</li>
+                            <li><Link to="https://jalajpandey.netlify.app/">Portfolio</Link></li>                            <li><Link to="https://jalaj-textwiz.netlify.app/">TextWiz App</Link></li>
+                           <li><Link to="https://jalaj-pandey.github.io/DrumKit/">DrumApp</Link></li>
                         </ul>
                         <ul className="box input-box">
-                            <li className="link_name">About iNotebook</li>
+                            <li className="link_name">About ideaSpotter</li>
                             <li style={{ color: "#F7FFFF" }}>
                                 An online web platform where you can create, edit, upload, delete your notes/information privately and securely without any disturbancee
                             </li>
